af4837c6949cbd42372e178652fb4c718fba1a25
nadirhussainnn <nadirhussaintumrani@gmail.com>
2021-11-29 15:25:46 +0500

    Done with fragments

--- Fragments_Starter/app/src/main/java/com/myloginsystem/fragments_starter/Fragment1.java
+++ Fragments_Starter/app/src/main/java/com/myloginsystem/fragments_starter/Fragment1.java
@@ -7,15 +7,34 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class Fragment1 extends Fragment {
 
 
+    Button btn;
+    TextView textView;
+    Boolean clicked=false;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_1, container, false);
+
+        View v=inflater.inflate(R.layout.fragment_1,container, false);
+        btn=v.findViewById(R.id.fragment1_btn);
+        textView=v.findViewById(R.id.msgText);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s=clicked?"Ok":"Cancel";
+                clicked=!clicked;
+                textView.setText(s);
+            }
+        });
+
+        return v;
     }
 }
